a7508e96789ab9bf73070a1e88401eb7e455f423
Yebbenbe <lmtpox@gmail.com>
2023-09-23 16:41:49 -0300

    assertEqual.js upload

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return ("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    return ("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
