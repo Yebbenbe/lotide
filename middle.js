53a9d8d047a4ac3b56e9fd2c99661551b31f0eb8
Yebbenbe <lmtpox@gmail.com>
2023-09-23 22:20:44 -0300

    added comments middle.js

--- middle.js
+++ middle.js
@@ -43,7 +43,7 @@ const middle = function(arr) {
   }
 };
 
-
+// Test cases using assertArraysEqual
 assertArraysEqual(middle([1, 2, 3]), [1, 2, 3]);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
