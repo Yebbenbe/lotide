a6ee62d8e72fc55fb674bbd14d5edd79dc2c8094
Yebbenbe <lmtpox@gmail.com>
2023-10-02 03:24:46 -0300

    updating assertEqual, upload findKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+// assertEqual.js
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKey scans obj and returns key matching the callback
+const findKey = function(obj, callback) {
+  // creates an array of the object's keys
+  const keys = Object.keys(obj);
+  for (let key of keys) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 2); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), undefined); // => "undefined"
