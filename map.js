5a5c3b199b821577de1e55cd65b112cc76b99938
Yebbenbe <lmtpox@gmail.com>
2023-10-02 03:11:56 -0300

    added map.js and takeUntil.js

---
+++ map.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  // checks to make sure both arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // iterates through array 1, comparing each element to array 2
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // If all elements are equal, return true
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  // runs eqArrays function. If true, areEqual === true. 
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+ 
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, word => word), ["ground", "control", "to", "major", "tom"]);
+assertArraysEqual(map(words, word => word[1]), ["r", "o", "o", "a", "o"]);
---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1, arr2) {
+  // checks to make sure both arrays are the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // iterates through array 1, comparing each element to array 2
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // If all elements are equal, return true
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  // runs eqArrays function. If true, areEqual === true. 
+  const areEqual = eqArrays(arr1, arr2);
+  if (areEqual) {
+    console.log(`Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+ 
+}
+
+// takeUntil
+const takeUntil = function(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ])
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ])
+
+console.log('---');
+const data3 = [0, 3, "number", 7, 5]
+const results3 = takeUntil(data3, x => x === 7);
+assertArraysEqual(results3, [0, 3, "number"]);
