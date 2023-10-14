a589c3b85198d806862510433c7f5982de6a7fd5
Yebbenbe <lmtpox@gmail.com>
2023-10-14 13:00:50 -0300

    linted

--- middle.js
+++ middle.js
@@ -2,15 +2,16 @@ const middle = function(arr) {
   const length = arr.length;
 
   if (length <= 2) {
-    return []; // No middle for arrays with one or two elements
+    // no middle for arrays with one or two elements
+    return []; 
   }
 
   if (length % 2 === 1) {
-    // Odd number of elements, return a single middle element
+    // odd number of elements, return a single middle element
     const middleIndex = Math.floor(length / 2);
     return [arr[middleIndex]];
   } else {
-    // Even number of elements, return two middle elements
+    // even number of elements, return two middle elements
     const middleIndex1 = length / 2 - 1;
     const middleIndex2 = length / 2;
     return [arr[middleIndex1], arr[middleIndex2]];
