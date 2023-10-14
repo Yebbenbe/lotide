28d727246530d995b62b92a0780c40344b3e2e4b
Yebbenbe <lmtpox@gmail.com>
2023-10-14 16:53:13 -0300

    corrected assertion dialogue for tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,11 +4,11 @@ const assert = require('chai').assert;
 const _ = require('../index');
 
 describe("#tail", () => {
-  it("returns [3] for [1, 2, 3]", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(_.tail([1, 2, 3]), [2, 3]);
   });
 
-  it("returns [c] for [a, b, c]", () => {
+  it("returns [b, c] for [a, b, c]", () => {
     assert.deepEqual(_.tail(["a", "b", "c"]), ['b', 'c']); 
   });
 
