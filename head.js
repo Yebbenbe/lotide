260b5303c5eb7e8b944678aa9430cfef8569b97a
Yebbenbe <lmtpox@gmail.com>
2023-10-14 12:38:19 -0300

    missed this in previous commit

--- head.js
+++ head.js
@@ -1,15 +1,10 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// assertEqual
+const assertEqual = require('./assertEqual');
+
+// giveHead 
 const head = function(array) {
   return array[0];
 }
 
-// test cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// exports the function
+module.exports = head;
