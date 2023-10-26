57fabe173d695364dd529ae1586c48df6bf3ac69
Yebbenbe <lmtpox@gmail.com>
2023-10-26 15:49:32 -0300

    add chai testing functionality

--- takeUntil.js
+++ takeUntil.js
@@ -1,30 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  // checks to make sure both arrays are the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // iterates through array 1, comparing each element to array 2
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // If all elements are equal, return true
-  return true;
-};
+// takeUntil.js should take in an array to work with and a callback function. The function will return a slice of the array with elements taken from the beginning. It should keep going until the callback/predicate returns a truthy value.
 
-const assertArraysEqual = function(arr1, arr2) {
-  // runs eqArrays function. If true, areEqual === true.
-  const areEqual = eqArrays(arr1, arr2);
-  if (areEqual) {
-    console.log(`Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
- 
-};
-
-// takeUntil
 const takeUntil = function(array, callback) {
   for (let i = 0; i < array.length; i++) {
     if (callback(array[i])) {
@@ -32,18 +7,5 @@ const takeUntil = function(array, callback) {
     }
   }
 };
-/*
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-
-console.log('---');
-const data3 = [0, 3, "number", 7, 5];
-const results3 = takeUntil(data3, x => x === 7);
-assertArraysEqual(results3, [0, 3, "number"]); */
+module.exports = takeUntil;
