dacf31a0197f17b5d88b404dd02f351a35ab2688
Yebbenbe <lmtpox@gmail.com>
2023-10-26 15:40:28 -0300

    add chai testing functionality

--- map.js
+++ map.js
@@ -1,30 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  // checks to make sure both arrays are the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // iterates through array 1, comparing each element to array 2
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  // If all elements are equal, return true
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  // runs eqArrays function. If true, areEqual === true.
-  const areEqual = eqArrays(arr1, arr2);
-  if (areEqual) {
-    console.log(`Assertion Passed: [${arr1}] === [${arr2}]`);
-  } else {
-    console.log(`Assertion Failed: [${arr1}] !== [${arr2}]`);
-  }
- 
-};
-
-const words = ["ground", "control", "to", "major", "tom"];
+// map.js should take an array to map and a callback function. It should return a new array based on the results of the callback function.
 
 const map = function(array, callback) {
   const results = [];
@@ -34,10 +8,4 @@ const map = function(array, callback) {
   return results;
 };
 
-//const results1 = map(words, word => word[0]);
-//console.log(results1);
-/*
-assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
-assertArraysEqual(map(words, word => word), ["ground", "control", "to", "major", "tom"]);
-assertArraysEqual(map(words, word => word[1]), ["r", "o", "o", "a", "o"]);
-*/
+module.exports = map;
