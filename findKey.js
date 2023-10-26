8127887f7a4775a6d5fc461dfc3ecd7043618dd5
Yebbenbe <lmtpox@gmail.com>
2023-10-26 15:08:35 -0300

    add findKey test

--- findKey.js
+++ findKey.js
@@ -1,11 +1,4 @@
-// assertEqual.js
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// findKey.js   
 
 // findKey scans obj and returns key matching the callback
 const findKey = function(obj, callback) {
@@ -18,22 +11,4 @@ const findKey = function(obj, callback) {
   }
 };
 
-/*
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 2); // => "noma"
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 4), undefined); // => "undefined"
-*/
+module.exports = findKey;
